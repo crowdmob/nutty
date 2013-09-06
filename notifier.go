601d0a528c22aca179cfc7d241699b62dc91759f
Matthew Moore <matt@crowdmob.com>
2013-09-05 21:05:49 -0700

    fix issue where subject waasn't output

--- notifier.go
+++ notifier.go
@@ -9,7 +9,7 @@ import (
 // Writes to SNS on the topic specified in nuttyApp.SnsArn if on production.  Otherwise, outputs with log.Println
 func (nuttyApp *App) SNSPublish(subject string, message string) {
 	go func() {
-		_, snsErr := sns.New(nuttyApp.AwsAuth, nuttyApp.AwsRegion).Publish(&sns.PublishOpt{message, "", fmt.Sprintf("[%s] %s", nuttyApp.Name), nuttyApp.SnsArn})
+		_, snsErr := sns.New(nuttyApp.AwsAuth, nuttyApp.AwsRegion).Publish(&sns.PublishOpt{message, "", fmt.Sprintf("[%s] %s", nuttyApp.Name, subject), nuttyApp.SnsArn})
 		if snsErr != nil {
 			log.Println(fmt.Sprintf("SNS error: %#v during report of error writing to kafka: %s", snsErr, message))
 		}
